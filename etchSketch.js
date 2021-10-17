974d7d204271cbe9b1e809b9603feb13eecaceed
Erik Borén <erik.boren@gmail.com>
2021-10-17 20:03:38 +0200

    Draw functionality and erase button added

--- etchSketch.js
+++ etchSketch.js
@@ -1,6 +1,34 @@
 var div = document.createElement("div")
 div.classList.add("box");
+const grid = document.getElementById("grid")
+const defaultGridSize = 16
+var gridSize = defaultGridSize;
 
-for (let i = 0; i < 20; i++) {
-  document.getElementById("box-container").appendChild(div);
+
+eraseGrid = function(){
+  grid.innerHTML ="";
 }
+
+changeColor = function(element){
+  element.target.style.backgroundColor = "black";
+}
+
+document.getElementById("erase").addEventListener('click', function(){
+  resizeGrid(gridSize);
+});
+
+resizeGrid = function(gridSize){
+  eraseGrid();
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for (let i = 0; i < gridSize*gridSize; i++) {
+    var div = document.createElement("div")
+    div.addEventListener('mouseover', changeColor);
+    div.classList.add("box");
+    grid.appendChild(div);
+  }
+
+}
+
+resizeGrid(gridSize);
